35e14b0890b66584f5c0b4035be99e93fa8c9822
sczerber <binarygun90@gmail.com>
2017-12-05 18:10:45 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(function () {
+    $('.slide__slider').slick({
+            autoplay: true,
+            //
+            dots: false,
+            // speed: 800,
+            //
+            // prevArrow: '<i class="prewArrow"></i>',
+            // nextArrow: '<i class="nextArrow"></i>',
+            // arrows: true
+        }
+    );
+});
